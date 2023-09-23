e6ec25f7cb6df969bcd8a2a07df518cf5d409ce4
Silas Rodrigues <silasrodrigues.fatec@gmail.com>
2023-09-22 21:32:18 -0300

    refactor: translate db seed to english

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -6,23 +6,23 @@ async function main() {
 
   await prisma.prompt.create({
     data: {
-      title: 'Título YouTube',
-      template: `Seu papel é gerar três títulos para um vídeo do YouTube.
+      title: 'YouTube Title',
+      template: `Your role is to generate three titles for a YouTube video.
 
-Abaixo você receberá uma transcrição desse vídeo, use essa transcrição para gerar os títulos.
-Abaixo você também receberá uma lista de títulos, use essa lista como referência para os títulos a serem gerados.
+Below you will receive a transcription of that video, use this transcription to generate the titles.
+Below you will also receive a list of titles, use this list as a reference for the titles to be generated.
 
-Os títulos devem ter no máximo 60 caracteres.
-Os títulos devem ser chamativos e atrativos para maximizar os cliques.
+The titles should be a maximum of 60 characters.
+The titles should be catchy and attractive to maximize clicks.
 
-Retorne APENAS os três títulos em formato de lista como no exemplo abaixo:
+Return ONLY the three titles in a list format as shown below:
 '''
-- Título 1
-- Título 2
-- Título 3
+- Title 1
+- Title 2
+- Title 3
 '''
 
-Transcrição:
+Transcription:
 '''
 {transcription}
 '''`.trim(),
@@ -31,26 +31,26 @@ Transcrição:
 
   await prisma.prompt.create({
     data: {
-      title: 'Descrição YouTube',
+      title: 'YouTube Description',
       template:
-        `Seu papel é gerar uma descrição sucinta para um vídeo do YouTube.
-  
-Abaixo você receberá uma transcrição desse vídeo, use essa transcrição para gerar a descrição.
+        `Your role is to generate a concise description for a YouTube video.
 
-A descrição deve possuir no máximo 80 palavras em primeira pessoa contendo os pontos principais do vídeo.
+Below you will receive a transcription of that video, use this transcription to generate the description.
 
-Use palavras chamativas e que cativam a atenção de quem está lendo.
+The description should have a maximum of 80 words in first person containing the main points of the video.
 
-Além disso, no final da descrição inclua uma lista de 3 até 10 hashtags em letra minúscula contendo palavras-chave do vídeo.
+Use catchy words that captivate the attention of the reader.
 
-O retorno deve seguir o seguinte formato:
+Additionally, at the end of the description include a list of 3 to 10 hashtags in lowercase containing video keywords.
+
+The return should follow the format below:
 '''
-Descrição.
+Description.
 
 #hashtag1 #hashtag2 #hashtag3 ...
 '''
 
-Transcrição:
+Transcription:
 '''
 {transcription}
 '''`.trim(),
